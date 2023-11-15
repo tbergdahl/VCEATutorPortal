0aeb2b876a4e2672c30417e2905149b70fc0ff53
adam.caudle <adam.caudle@wsu.edu>
2023-11-15 12:16:36 -0800

    attempted testing changes, no progress

--- TutorProject/Apps/TutorApp/tests/test_tutor.py
+++ TutorProject/Apps/TutorApp/tests/test_tutor.py
@@ -3,10 +3,17 @@ import os
 from django.test import TestCase, override_settings
 from django.urls import reverse
 from Apps.TutorApp.models import *  # Import your models from the correct location
+from django.db import transaction
 
 warnings.filterwarnings("ignore")
 
 class TestTutor(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Constant test data here
+        pass
+
+    @transaction.atomic
     def setUp(self):
         # Create a test user
         self.tutor_user = CustomUser.objects.create(
@@ -17,54 +24,58 @@ class TestTutor(TestCase):
             is_student=False,
             is_admin=False
         )
-        self.tutor_user.save()
 
-        self.Tutor = Tutor(
+        # Create a new Tutor instance for each test
+        self.tutor = Tutor(
             user=self.tutor_user,
             minutes_tutored=0,
             day_started=None,
             rating=0.0,
             description='Legendary bodybuilder and actor'
         )
-        self.Tutor.save()
+        print(Tutor.objects.all())
+        self.tutor.save()
+        print(Tutor.objects.all())
 
+    @transaction.atomic
     def tearDown(self):
-        # Delete the created objects to clean up
-        CustomUser.objects.all().delete()
+        # Clean up the created objects
         Tutor.objects.all().delete()
-
+        CustomUser.objects.all().delete()
+        
 
     def test_assignment(self):
         # Test tutor assignment
-        self.assertFalse(self.Tutor.user.is_student)
-        self.assertFalse(self.Tutor.user.is_admin)
-        self.assertTrue(self.Tutor.user.is_tutor)
+        self.assertFalse(self.tutor.user.is_student)
+        self.assertFalse(self.tutor.user.is_admin)
+        self.assertTrue(self.tutor.user.is_tutor)
 
         # Test tutor information
-        self.assertEqual(self.Tutor.user.email, 'jay.cutler1@wsu.edu')
-        self.assertEqual(self.Tutor.user.first_name, 'Jay')
-        self.assertEqual(self.Tutor.user.last_name, 'Cutler')
-        self.assertEqual(self.Tutor.minutes_tutored, 0)
-        self.assertEqual(self.Tutor.day_started, None)
-        self.assertEqual(self.Tutor.rating, 0.0)
-        self.assertEqual(self.Tutor.description, 'Legendary bodybuilder and actor')
+        self.assertEqual(self.tutor.user.email, 'jay.cutler1@wsu.edu')
+        self.assertEqual(self.tutor.user.first_name, 'Jay')
+        self.assertEqual(self.tutor.user.last_name, 'Cutler')
+        self.assertEqual(self.tutor.minutes_tutored, 0)
+        self.assertEqual(self.tutor.day_started, None)
+        self.assertEqual(self.tutor.rating, 0.0)
+        self.assertEqual(self.tutor.description, 'Legendary bodybuilder and actor')
 
     def test_rating(self):
-        test_feedback = Feedback(
-            tutor=self.Tutor,
+        print(Tutor.objects.all())
+        # Save the tutor instance to the database
+        self.tutor.save()
+
+        # Create feedback instances for testing
+        test_feedback = Feedback.objects.create(
+            tutor=self.tutor,
             rating=5,
             feedback="Great tutor"
         )
-        test_feedback_2 = Feedback(
-            tutor=self.Tutor,
+        test_feedback_2 = Feedback.objects.create(
+            tutor=self.tutor,
             rating=0,
             feedback="Bad tutor"
         )
 
-        test_feedback.save()
-        test_feedback_2.save()
-
-        self.Tutor.compute_rating()
-        self.assertEqual(self.Tutor.rating, 2.5)
-
-
+        # Compute the tutor's rating
+        self.tutor.compute_rating()
+        self.assertEqual(self.tutor.rating, 2.5)
