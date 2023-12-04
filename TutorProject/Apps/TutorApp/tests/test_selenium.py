0f3d0c649f6c667a90a3af6e391677b4196525c2
adam.caudle <adam.caudle@wsu.edu>
2023-12-03 16:20:39 -0800

    Selenium testing for very basic student user cases

--- TutorProject/Apps/TutorApp/tests/test_selenium.py
+++ TutorProject/Apps/TutorApp/tests/test_selenium.py
@@ -8,6 +8,12 @@ from django.contrib.auth import get_user_model
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
+
+from selenium.common.exceptions import TimeoutException
+
+class AppointmentsLinkNotFoundError(TimeoutException):
+    pass
 class MySeleniumTests(LiveServerTestCase):
     @classmethod
     def setUpClass(cls):
@@ -20,7 +26,8 @@ class MySeleniumTests(LiveServerTestCase):
         super().tearDownClass()
 
     def test_login(self):
-        # Create a student user
+
+        # Create a student user and student
         user_model = get_user_model()
         student_user = user_model.objects.create_user(
             email='ronnie.coleman@wsu.edu',
@@ -30,7 +37,26 @@ class MySeleniumTests(LiveServerTestCase):
             is_admin=False
         )
         student = Student(user=student_user)
+
+        # Create a tutor user and tutor
+        tutor_user = user_model.objects.create(
+            email='jay.cutler1@wsu.edu',
+            first_name='Jay',
+            last_name='Cutler',
+            is_tutor=True,
+            is_student=False,
+            is_admin=False
+        )
+        tutor = Tutor(
+            user=tutor_user,
+            hours_tutored=0,
+            day_started=None,
+            rating=0.0,
+            description='Legendary bodybuilder and actor'
+        )
+
         student.save()
+        tutor.save()
 
         # Open the selenium browser
         self.selenium.get(self.live_server_url)
@@ -40,6 +66,7 @@ class MySeleniumTests(LiveServerTestCase):
             EC.presence_of_element_located((By.NAME, "username"))
         )
 
+        time.sleep(1) # wait for bettter visibility
         # Continue with your test
         email_input.send_keys("ronnie.coleman@wsu.edu")
 
@@ -50,4 +77,29 @@ class MySeleniumTests(LiveServerTestCase):
         # Click the login button
         login_button = self.selenium.find_element(By.CSS_SELECTOR, ".loginPage-submit-button")
         login_button.click()
+
+        # Wait for the student page to load
+        student_page_title = WebDriverWait(self.selenium, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//h2[text()='Rate your Tutors']"))
+        )
+        time.sleep(1) # wait for bettter visibility
+
+
+        # Check if the tutor list is present
+        tutor_list = self.selenium.find_elements(By.CLASS_NAME, "student-tutor-card")
+        self.assertTrue(len(tutor_list) > 0, "Tutor list is empty")
+
+        time.sleep(1) # wait for bettter visibility
+
+        # Click on the first tutor
+        first_tutor = tutor_list[0]
+        first_tutor.find_element(By.CLASS_NAME, "student-action-button").click()
+        
+        # After viewing the tutor, navigate to the Appointments section
+        time.sleep(1) # wait for bettter visibility
+
+        self.selenium.get(self.live_server_url + "/appointments/1/")  # Update the URL with the correct path
+
+        
+
         
